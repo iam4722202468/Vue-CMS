7f6c461bbc5d9feb456dfa04356e819feb771795
iam4722202468 <aceparent@gmail.com>
2019-08-19 18:00:01 -0400

    fix data folder

--- src/router.js
+++ src/router.js
@@ -4,57 +4,13 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: paths.map(i => (
     {
-      path: '/',
-      name: 'Home',
+      path: '/' + i.replace('home.vue', '').replace('.vue',''),
+      name: i.replace('.vue',''),
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/Home.vue')
-    },
-    {
-      path: '/about2',
-      name: 'about2',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/About2.vue')
-    },
-    {
-      path: '/lightingengine',
-      name: 'Lighting Engine',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/lightingengine.vue')
-    },
-    {
-      path: '/montecarlotree',
-      name: 'Monte Carlo Tree',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/montecarlotree.vue')
-    },
-    {
-      path: '/scheduler',
-      name: 'Guelph scheduler 2',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/guelphscheduler2.vue')
-    },
-    {
-      path: '/co-op',
-      name: 'co-op_1',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/co-op.vue')
-    },
-    {
-      path: '/co-op2',
-      name: 'co-op_2',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/co-op2.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/About.vue')
-    },
-    {
-      path: '/resume',
-      name: 'Resume',
-      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/resume.vue')
+      component: () => import(/* webpackChunkName: "about" */ '../Vue-CMS-Data/views/' + i)
     }
-  ]
-})
+  ))})
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,27 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path')
+
+const isDirectory = source => lstatSync(source).isFile()
+const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isFile())
+
+const someFileContents = JSON.stringify(dirs('./Vue-CMS-Data/views'))
+
 module.exports = {
   lintOnSave: false,
-  assetsDir: "Vue-CMS-Data",
+  configureWebpack: {
+    performance: {
+      maxAssetSize: 500000,
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: 'Vue-CMS-Data/public', to: '' }]),
+      new webpack.DefinePlugin({
+          paths: someFileContents,
+        })
+    ],
+  },
   devServer: {
     proxy: {
       '/api': {
